eb9ac317201543038e99c88ba5990c37987c8ba6
josematr <146174935+josematr@users.noreply.github.com>
2026-02-24 22:07:39 +0100

    Create enumeration playbook in 01-enum.ps1
    Added a PowerShell enumeration playbook for post-exploitation.

--- 02-resource-development-initial-access/Ejemplos/01-enum.ps1
+++ 02-resource-development-initial-access/Ejemplos/01-enum.ps1
@@ -1,1 +1,14 @@
-a
+# PowerShell Enumeration Playbook
+
+## Objetivo
+
+Utilizar PowerShell para realizar enumeración post-explotación en un sistema Windows comprometido utilizando únicamente herramientas nativas (LOTL - Living Off The Land).
+
+---
+
+# 1. Información del Sistema Operativo
+
+## Obtener información detallada del SO
+
+```powershell
+Get-WmiObject -Class Win32_OperatingSystem | Format-List *
